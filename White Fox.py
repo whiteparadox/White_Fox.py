dbcdf4b4c28f92bd08a2d227eb10cbb3cb5acfd9
whiteparadox <subigayen123@gmail.com>
2024-05-21 23:16:35 +0530

    Initial commit

---
+++ White Fox.py
@@ -0,0 +1,109 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[0].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning Sir!")
+
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon Sir!")
+
+    else:
+        speak("Good Evening Sir!")
+
+    speak("I am white Fox. sir, Please Tell me How may i help you")
+
+def takeCommand():
+    # it takes microphone from users and speak the string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio,Language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('subigayen123@gmail.com', 'your-password')
+    server.sendmail('gjcsubigayen123@gmail.com', to, content)
+    server.close()
+
+if __name__ == '__main__':
+    wishMe()
+    # while True:
+    if 1:
+        query = takeCommand().lower()
+
+     # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak ('Searching wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak ("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+
+        elif 'play music' in query:
+            music_dir = 'D:\\Music\\Fav'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, The time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Maa Anima\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'email to subimal' in query:
+            try:
+                speak("what should i say?")
+                content = takeCommand()
+                to = "subigayen123@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry sir. I am not able to send this email")
